77a2e97e2966b70e97413e09fe75175a5bddf428
장지은 <3jine33@naver.com>
2021-06-10 10:35:20 +0900

    feat : footer 위치 수정

--- react/conversefront/src/pages/MainPage.js
+++ react/conversefront/src/pages/MainPage.js
@@ -2,15 +2,16 @@ import React from "react";
 import { ContentBlock, Explore, Footer, Header } from "../components/templates";
 import Container from "@material-ui/core/Container";
 
+
 const MainPage = () => {
   return (
     <>
       <Header />
-      <Container maxWidth="xl">
-        <ContentBlock />
+      <Container maxWidth="xl" style={{"top":"300px"}}>
+        <ContentBlock  />
         <Explore/>
-        <Footer/>
       </Container>
+      <Footer/>
     </>
   );
 };
