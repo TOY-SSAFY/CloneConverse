e51d065aab5d25b43cb888eafc8de272aa319756
전헤민 <wlalsdns@naver.com>
2021-06-22 15:34:39 +0900

    feat: response객체

---
+++ src/main/java/com/ssafy/cloneconverse/configuration/response/RestResponse.java
@@ -0,0 +1,16 @@
+package com.ssafy.cloneconverse.configuration.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestResponse<T> {
+    private int code;
+    private String message;
+    private T result;
+}
